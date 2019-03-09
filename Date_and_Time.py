df93ef977f103c730d4c17c5a136ee0ba1089962
sriivaishnavi <sriivaishnavi@gmail.com>
2019-03-08 20:24:31 -0800

    Add files via upload

---
+++ Date_and_Time.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import datetime
+
+now = datetime.datetime.now()
+
+
+print("Current date and time using str method of datetime object:")
+print(str(now))
+
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.microsecond)
+
+
+# In[ ]:
+
+
+
+
